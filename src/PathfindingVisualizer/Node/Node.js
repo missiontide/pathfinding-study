63ea6378d1a3b94ec104dcc9973cb602c490c145
missiontide <patcoronel@missiontide.com>
2022-05-18 18:00:23 -0700

    initial grid and styling completed

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -5,13 +5,26 @@ import './Node.css'
 export default class Node extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            row: this.props.row,
+            col: this.props.col,
+            isStart: this.props.isStart,
+            isFinish: this.props.isFinish,
+        };
     }
 
     render () {
+        const {isStart, isFinish} = this.props;
+        const extraClassName =
+            isStart ? 'node-start'
+            : isFinish ? 'node-finish'
+            : '';
+
         return (
-            <div className="node">
-                Node
+            <div
+                className={`node ${extraClassName}`}
+                key={this.key}>
+
             </div>
         )
     }
--- src/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -7,13 +7,53 @@ import './PathfindingVisualizer.css'
 export default class PathfindingVisualizer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nodes: [],
+            rows: 25,
+            cols: 50,
+            startNode: {row: 5, col: 5},
+            finishNode: {row: 20, col: 40},
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < this.state.rows; row++) {
+            const currentRow = [];
+            for (let col = 0; col < this.state.cols; col++) {
+                const currentNode = {
+                    row,
+                    col,
+                    isStart: row === this.state.startNode.row && col === this.state.startNode.col,
+                    isFinish: row === this.state.finishNode.row && col === this.state.finishNode.col,
+                };
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
     }
 
     render () {
+        const {nodes} = this.state;
+
         return (
-            <div>
-                <Node />
+            <div className="gridWrapper">
+                {nodes.map((row, rowIdx) => {
+                    return (
+                        <div className="row" key={rowIdx}>
+                            {row.map((node, nodeIdx) =>
+                                <Node
+                                    key={node.row + "-" + node.col}
+                                    row={node.row}
+                                    col={node.col}
+                                    isStart={node.isStart}
+                                    isFinish={node.isFinish}
+                                />
+                            )}
+                        </div>
+                    )
+                })}
             </div>
         )
     }
